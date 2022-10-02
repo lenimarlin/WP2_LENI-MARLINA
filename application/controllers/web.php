a35532e5ed007e25652fe3decad37e6efabaab03
lenimarlin <leni23273@gmail.com>
2022-10-02 22:07:53 +0700

    tugas pert3

---
+++ application/controllers/web.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') or exit ('no direct script access allowed');
+
+class web extends CI_Controller{
+
+    function __construct(){
+        parent::__construct();
+        $this->load->helper('url');
+    }
+
+    
+    public function about()
+        {
+
+            $data['judul'] = "Halaman About";
+            $this->load->view('v_header', $data);
+            $this->load->view('v_about', $data);
+            $this->load->view('v_footer', $data);
+        }
+
+    }
+
+
---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>nama depan : leni</li>
+        <li>Nama Belakang : marlina</li>
+    </ul>
+    <br>
+    <h4>alamat</h4>
+    <ul type="none">
+        <li> jalan perdamaiyan</li>
+    </ul>
+
+    <h4>tempat lahir </h4>
+    <ul type="none">
+        <li>pontianak</li>
+    </ul>
+
+    <h4>olahraga favorit</h4>
+    <ul type="sqare">
+        <li>bulutangkis</li>
+        <li>voly</li>
+    </ul>
+</section>
